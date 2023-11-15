0d3bc2342a47d56333288daeda836e2f52b4fb44
André <120421201+WedSan@users.noreply.github.com>
2023-11-15 15:45:43 -0300

    Create GameRepository

---
+++ src/main/java/com/project/gameapi/repository/GameRepository.java
@@ -0,0 +1,10 @@
+package com.project.gameapi.repository;
+
+import com.project.gameapi.model.Game;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GameRepository extends JpaRepository<Game, Long> {
+
+}

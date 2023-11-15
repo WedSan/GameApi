32a009cfab5e368e781696a65abca4932b25a398
André <120421201+WedSan@users.noreply.github.com>
2023-11-15 17:13:39 -0300

    Create GameRepository interface
    - GameRepository interface
    - Custom methods for checking existence by name and retrieving by ID

--- src/main/java/com/project/gameapi/repository/GameRepository.java
+++ src/main/java/com/project/gameapi/repository/GameRepository.java
@@ -4,7 +4,10 @@ import com.project.gameapi.model.Game;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface GameRepository extends JpaRepository<Game, Long> {
-
+    boolean existsByName(String name);
+    Optional<Game> findById(Long id);
 }

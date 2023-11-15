6f745a5f5945be8299c7fe5796ffdf3be4cacafa
André <120421201+WedSan@users.noreply.github.com>
2023-11-15 17:17:16 -0300

    Implement Game entity with ReqGame and ReqUpdateGame integration
    - Game entity with JPA annotations
    - Constructors, getters, and setters for the Game entity
    - createGame method for initializing the Game entity from a ReqGame instance
    - update method for modifying the Game entity based on a ReqUpdateGame instance

--- src/main/java/com/project/gameapi/model/Game.java
+++ src/main/java/com/project/gameapi/model/Game.java
@@ -1,9 +1,7 @@
 package com.project.gameapi.model;
 
 import jakarta.persistence.*;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.math.BigDecimal;
 
@@ -11,12 +9,32 @@ import java.math.BigDecimal;
 @Table(name = "tb_game")
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @EqualsAndHashCode(of = "id")
 public class Game {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String nome;
+    private String name;
     private String description;
     private BigDecimal price;
+
+    public Game(ReqGame reqGame){
+        this.name = reqGame.name();
+        this.description = reqGame.description();
+        this.price = reqGame.price();
+    }
+
+    public void update(ReqUpdateGame reqGame){
+        if(reqGame.name()!=null){
+            this.name = reqGame.name();
+        }
+        if(reqGame.description()!=null){
+            this.description = reqGame.description();
+        }
+        if(reqGame.price()!=null){
+            this.price = reqGame.price();
+        }
+    }
 }
